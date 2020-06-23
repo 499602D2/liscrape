0e37fd6ce77ce42ae117a5c08b72e2c1a9f35970
Daniel V <46869890+499602D2@users.noreply.github.com>
2020-06-23 12:45:17 +0300

    Create liscrape.py

---
+++ liscrape.py
@@ -0,0 +1,9 @@
+'''
+Basic idea:
+	1. find an interesting profile
+	2. drag link into UI
+	3. info automatically pulled from Linkedin and inserted into a CSV for automatic import into CRM
+'''
+
+if __name__ == '__main__':
+	pass
