626512dd8878224106ea30c7c616cd4686064f93
Daniel V <46869890+499602D2@users.noreply.github.com>
2020-06-25 16:11:25 +0300

    Update liscrape.py

--- liscrape.py
+++ liscrape.py
@@ -1,3 +1,4 @@
+import requests
 import PySimpleGUI as sg
 
 '''
@@ -6,27 +7,67 @@ Basic idea:
 	2. drag link into UI
 	3. info automatically pulled from Linkedin and inserted into a CSV for automatic import into CRM
 '''
+class Session:
+	'''
+	Session stores the current session's log-in cookie, among other things
+	'''
+	def __init__(self):
+		self.username = None
+		self.password = None
+
+	def sign_in(self, username, password):
+		self.username = username
+		self.password = password
+
+	def display_signin_screen(self, VERSION):
+		layout = [
+			[sg.Text('Sign in to LinkedIn to continue')],
+			[sg.Text('Username (email)\t'), sg.InputText(key="username")],
+			[sg.Text('Password\t\t'), sg.InputText(key="password")],
+			[sg.Button('Sign in')],
+			[sg.Output(size=(60, 20))]
+		]
+
+		return sg.Window(f'Liscrape version {VERSION}', layout)
+
 
 if __name__ == '__main__':
 	VERSION = 0.1
+	headers = {
+		"user-agent": 
+			"Mozilla/5.0 \
+			(Macintosh; Intel Mac OS X 10_13_5) \
+			AppleWebKit/537.36 (KHTML, like Gecko) \
+			Chrome/66.0.3359.181 \
+			Safari/537.36"
+	}
+	
+	# set theme
+	sg.theme('Reddit')
 
-	# All the stuff inside your window.
-	layout = [
-				[sg.Text('Drag Linkedin profile link into the form beow')],
-				[sg.Text('Linkedin profile URL\t\t'), sg.InputText()],
-				[sg.Text('File the data is saved to\t'), sg.InputText()],
-				[sg.Button('Process'), sg.Button('Cancel')] ]
-
-	# Create the Window
-	window = sg.Window(f'Liscrape {VERSION}', layout)
+	# create session, display sign-in screen
+	session = Session()
+	window = session.display_signin_screen(VERSION)
 
 	# Event Loop to process "events" and get the "values" of the inputs
 	while True:
-	    event, values = window.read()
-	    
-	    if event == sg.WIN_CLOSED or event == 'Cancel':	# if user closes window or clicks cancel
-	        break
+		event, values = window.read()
+		print(f'event: {event} | values: {values}')
 
-	    print('You entered ', values[0])
+		if event == sg.WIN_CLOSED:
+			break
+
+		elif event == 'Sign in':
+			if values['username'] != '' and values['password'] != '':
+				session.sign_in(values['username'], values['password'])
+				print('Signing in...')
+			else:
+				sg.popup('Please enter your login details!')
+
+		print('You entered ', values)
+
 
 	window.close()
+
+
+
