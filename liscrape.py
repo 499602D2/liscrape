21cf413369ff30cd021c901dd628c7894b0773df
Daniel V <46869890+499602D2@users.noreply.github.com>
2020-06-27 11:40:17 +0300

    Update liscrape.py
    Session class:
    
    - add window property
    
    - add sheet_path property
    
    - add parsed property
    
    General:
    
    - add configuration for storing login information
    
    - add option to store login and refresh cookies
    
    - add option to choose excel sheet
    
    - add main UI

--- liscrape.py
+++ liscrape.py
@@ -1,7 +1,6 @@
-import requests
+import os, json
 import PySimpleGUI as sg
 
-from pprint import pprint
 from linkedin_api import Linkedin
 
 '''
@@ -9,6 +8,9 @@ Basic idea:
 	1. find an interesting profile
 	2. drag link into UI
 	3. info automatically pulled from Linkedin and inserted into a CSV for automatic import into CRM
+
+To-do
+	Package with PyInstaller
 '''
 class Session:
 	'''
@@ -18,15 +20,66 @@ class Session:
 		self.username = None
 		self.password = None
 		self.authenticated = False
+		self.window = None
+		self.sheet_path = None
+		self.parsed = 0
 
-	def sign_in(self, username, password):
+	def load_configuration(self):
+		if not os.path.isfile('config.json'):
+			return []
+
+		with open('config.json', 'r') as config_file:
+			try:
+				config = json.load(config_file)
+			except:
+				os.remove('config.json')
+				return ()
+
+			return tuple(config['users'].keys()) if len(config['users'].keys()) > 0 else ()
+
+	def load_password_from_config(self, username):
+		with open('config.json', 'r') as config_file:
+			config = json.load(config_file)
+
+		try:
+			return config['users'][username]
+		except:
+			sg.popup('Error finding password from configuration!')
+			raise Exception('Error finding password from configuration!')
+
+	def store_login(self, username, password):
+		if not os.path.isfile('config.json'):
+			with open('config.json', 'w') as config_file:
+				config = {}
+				config['users'] = {}
+		else:
+			with open('config.json', 'r') as config_file:
+				config = json.load(config_file)
+
+		config['users'][username] = password
+		with open('config.json', 'w') as config_file:
+			json.dump(config, config_file, indent=4)
+
+		return True
+
+	def sign_in(self, username, password, remember_login, refresh_cookies):
 		self.username = username
 		self.password = password
-		return self.authenticate()
+		auth_success = self.authenticate(refresh_cookies)
 
-	def authenticate(self):
+		# DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG 
+		self.authenticated = True
+		print(f'remember_login: {remember_login}, type: {type(remember_login)}')
+		if self.authenticated and remember_login:
+			success = self.store_login(username, password)
+			if success:
+				print('Login stored into config file successfully!')
+
+		return auth_success
+
+	def authenticate(self, refresh_cookies):
 		try:
-			api = Linkedin(self.username, self.password)
+			api = Linkedin(self.username, self.password, debug=True, refresh_cookies=refresh_cookies)
 			self.authenticated = True
 			print(f'api: {api}')
 			return api
@@ -35,65 +88,139 @@ class Session:
 			if 'BAD_EMAIL' in error_args:
 				sg.popup('Incorrect email: try again.')
 			elif 'CHALLENGE' in error_args:
-				sg.popup('Challenge')
+				sg.popup('Error: LinkedIn requires a sign-in challenge.')
 			else:
-				sg.popup(error_args)
+				print(error)
+				print(error_args)
+				sg.popup(f'Unhandled exception: {error_args}')
 
 			return False
 
+	def store_contact_information(self, contact_dict):
+		pass
+
 	def display_signin_screen(self, VERSION):
 		layout = [
-			[sg.Text('Sign in to LinkedIn to continue')],
-			[sg.Text('Username (email)\t'), sg.InputText(key="username")],
-			[sg.Text('Password\t\t'), sg.InputText(key="password")],
-			[sg.Button('Sign in')],
-			[sg.Output(size=(60, 20))]
+			[sg.Text('Sign in to LinkedIn to continue', font=('Helvetica Bold', 14))],
+			[sg.Text('Username (email)\t', font=('Helvetica', 14)), sg.InputText(key="username")],
+			[sg.Text('Password\t\t', font=('Helvetica', 14)), sg.InputText(key="password")],
+			[	
+				sg.Text('Select a stored login', font=('Helvetica', 14)),
+				sg.Listbox(
+					self.load_configuration(), select_mode='LISTBOX_SELECT_MODE_SINGLE', 
+					enable_events=True, size=(40, 1 + len(self.load_configuration())),
+					key='-USERNAME-'
+					)
+			],
+			[
+				sg.Button('Sign in', font=('Helvetica', 14)), 
+				sg.Checkbox('Remember me', key='remember'),
+				sg.Checkbox('Refresh cookies', key='cookies')]
+		] # [sg.Output(size=(80, 20))]
+
+		return sg.Window(f'Liscrape version {VERSION}', layout=layout, resizable=True, grab_anywhere=True)
+
+	def display_sheet_screen(self, VERSION):
+		layout = [
+			[sg.Text('Choose the Excel sheet to store contact information in', font=('Helvetica Bold', 14))],
+			[sg.FileBrowse(), sg.Input(key="sheet_path")],
+			[sg.Button('OK')]
 		]
 
 		return sg.Window(f'Liscrape version {VERSION}', layout)
 
+	def display_main_screen(self, VERSION):
+		layout = [
+			[sg.Text(f'Signed in as {self.username}', font=('Helvetica', 14))],
+			[sg.Text('Contact to store (URL)\t', font=('Helvetica', 14)), sg.InputText(key="profile_url")],
+			[sg.Button('Store contact', font=('Helvetica', 14)), sg.Text(f'{self.parsed} contacts stored')],
+			[sg.Text(f'Storing into {self.sheet_path}', font=('Helvetica', 12))]
+		] # [sg.Output(size=(60, 20))]
+
+		return sg.Window(
+			title=f'Liscrape version {VERSION}', layout=layout, resizable=True, grab_anywhere=True)
+
 
 if __name__ == '__main__':
-	VERSION = 0.1
-	headers = {
-		"user-agent": 
-			"Mozilla/5.0 \
-			(Macintosh; Intel Mac OS X 10_13_5) \
-			AppleWebKit/537.36 (KHTML, like Gecko) \
-			Chrome/66.0.3359.181 \
-			Safari/537.36"
-	}
+	VERSION = 0.2
 	
 	# set theme
 	sg.theme('Reddit')
 
 	# create session, display sign-in screen
 	session = Session()
-	window = session.display_signin_screen(VERSION)
+	session.window = session.display_signin_screen(VERSION)
 
-	# Event Loop to process "events" and get the "values" of the inputs
+	# sign-in eventloop
 	while True:
-		event, values = window.read()
+		event, values = session.window.read()
 		print(f'event: {event} | values: {values}')
 
 		if event == sg.WIN_CLOSED:
 			break
 
-		elif event == 'Sign in':
-			if values['username'] != '' and values['password'] != '':
-				api = session.sign_in(values['username'], values['password'])
+		if event == 'Sign in':
+			if values['username'] != '' and values['password'] != '' or values['-USERNAME-'] != []:
+				if values['-USERNAME-'] != []:
+					username = values['-USERNAME-'][0]
+					password = session.load_password_from_config(username)
+					values['remember'] = False
+
+				api = session.sign_in(values['username'], values['password'], values['remember'], values['cookies'])
 				print('Signing in...')
 
 				if api is False:
 					sg.popup('Incorrect login details!')
+				else: 
+					# successful sign-in, update UI
+					sg.popup('Signed in successfully!')
+					session.window.close()
 
+					# request sheet/csv location
+					session.window = session.display_sheet_screen(VERSION)
+					while session.sheet_path == None:
+						event, values = session.window.read()
+						print(f'event: {event} | values: {values}')
+						if event == sg.WIN_CLOSED:
+							break
+
+						if values['sheet_path'] != '':
+							if '.xls' in values['sheet_path'] or '.xlsx' in values['sheet_path']:
+								session.sheet_path = values['sheet_path']
+
+					session.window.close()
+
+					# main screen
+					session.window = session.display_main_screen(VERSION)
+					break
 			else:
 				sg.popup('Please enter your login details!')
 
 		print('You entered ', values)
 
+	if session.sheet_path == None:
+		session.window.close()
+		sg.popup('Error: no Excel sheet path defined!')
 
-	window.close()
+	# main eventloop
+	while True:
+		event, values = session.window.read()
+		if event == sg.WIN_CLOSED:
+			break
+
+		if 'profile_url' in values:
+			print(f'Loading {values["profile_url"]}...')
+			profile = values['profile_url'].split('/')[-1]
+
+			profile = api.get_profile(profile)
+			print(profile)
+			print('\n\n')
+
+			contact_info = api.get_profile_contact_info(profile)
+			print(contact_info)
+
+
+	session.window.close()
 
 
 
