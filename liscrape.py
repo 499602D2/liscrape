08ec337622ebbe3166e8044cc928f35bbd347423
Daniel V <46869890+499602D2@users.noreply.github.com>
2020-06-29 18:08:32 +0300

    Version 1.0.0
    This is effectively the first working version of this program: version number has been bumped to 1.0.0 accordingly.
    
    - Reverted back to the old, unofficial API
    
    - Implement history for imposing API limitations and ignoring duplicates
    
    - Added logging, traceback
    
    - Fully implemented csv writing and profile json parsing

--- liscrape.py
+++ liscrape.py
@@ -1,26 +1,92 @@
-# -*- coding: utf-8 -*-
-import os, json
+import os, json, csv, time, logging, traceback
 import PySimpleGUI as sg
-from linkedin_v2 import linkedin
+from linkedin_api import Linkedin
 
-'''
-To-do
-	Package with PyInstaller
-'''
-class Session:
+class History:
 	def __init__(self):
-		self.application_name = None
+		self.call_count = 0
+		self.hourly_limit = 60
+		self.history = {}
+
+	def load(self):
+		if not os.path.isfile('config.json'):
+			with open('config.json', 'w') as config_file:
+				config = {'users': {}, 'history': {}}
+				json.dump(config, config_file, indent=4)
+			
+			return {}
+
+		with open('config.json', 'r') as config_file:
+			try:
+				config = json.load(config_file)
+				self.call_count = len(config['history'].keys())
+				return config['history']
+			except Exception as error:
+				logging.exception(error)
+				os.remove('config.json')
+				return {}
+
+	def store(self):
+		if os.path.isfile('config.json'):
+			with open('config.json', 'r') as config_file:
+				config = json.load(config_file)
+
+			with open('config.json', 'w') as config_file:
+				config['history'] = self.history
+				json.dump(config, config_file, indent=4)
+		else:
+			with open('config.json', 'w') as config_file:
+				config = {'users': {}, 'history': {}}
+				config['history'] = self.history
+				json.dump(config, config_file, indent=4)
+
+	def add(self, user_id):
+		self.call_count += 1
+		not_added = False if user_id in self.history.values() else True
+
+		self.history[time.time()] = user_id
+		return not_added
+
+	def check_validity(self):
+		if self.call_count > self.hourly_limit:
+			filtered_history = {}
+			for key, val in self.history.items():
+				if time.time() - float(key) <= 3600:
+					filtered_history[key] = val
+
+			self.call_count = len(filtered_history.keys())
+			if self.call_count > self.hourly_limit:
+				earliest_call = min([float(key) for key in self.history.keys()])
+				since_call = time.time() - earliest_call
+				return False, f'{int((3600 - since_call) / 60)} minutes'
+
+		return True, None
+
+class Session:
+	'''
+	Session stores the current session's log-in cookie, among other things
+	'''
+	def __init__(self):
+		self.version = '1.0.0'
+		self.username = None
+		self.password = None
 		self.authenticated = False
-		self.application = None
-		
-		self.consumer_key = None
-		self.consumer_secret = None
-		self.user_token = None
-		self.user_secret = None
-		
-		self.window = None
+		self.window = self.display_signin_screen()
 		self.sheet_path = None
+		self.total_parsed = 0
 		self.parsed = 0
+
+		self.history = History()
+		self.history.history = self.history.load()
+		self.history.check_validity()
+
+	def load_sheet_length(self):
+		if not os.path.isfile(self.sheet_path):
+			self.total_parsed = 0
+		else:
+			with open(self.sheet_path, 'r') as csv_file:
+				csv_reader =  csv.reader(csv_file)
+				self.total_parsed = sum(1 for row in csv_reader)
 
 	def load_configuration(self):
 		if not os.path.isfile('config.json'):
@@ -29,245 +95,273 @@ class Session:
 		with open('config.json', 'r') as config_file:
 			try:
 				config = json.load(config_file)
-			except:
+			except Exception as error:
+				logging.exception(error)
 				os.remove('config.json')
-				return []
+				return ()
 
-			keys = []
-			for key in config:
-				if config[key]['app_name'] != '':
-					keys.append(config[key]['app_name'])
-				else:
-					keys.append(key)
+			return tuple(config['users'].keys()) if len(config['users'].keys()) > 0 else ()
 
-			return keys if len(keys) > 0 else ()
-
-	def load_password_from_config(self, consumer_key):
+	def load_password_from_config(self, username):
 		with open('config.json', 'r') as config_file:
 			config = json.load(config_file)
 
-		# if key not found, we're using an app name
-		if consumer_key not in config.keys():
-			for key in config:
-				if config[key]['app_name'] == consumer_key:
-					return config[key]
-
 		try:
-			return config[consumer_key]
+			return config['users'][username]
 		except:
-			sg.popup('Error finding password from configuration!')
+			sg.popup('Error finding password from configuration!', title='Error', keep_on_top=True)
 			raise Exception('Error finding password from configuration!')
 
-	def store_login(self, app_name, key_dict):
-		consumer_key = key_dict['consumer_key']
+	def store_login(self, username, password):
 		if not os.path.isfile('config.json'):
 			with open('config.json', 'w') as config_file:
-				config = {}
-				config[consumer_key] = {}
+				config = {'users': {}, 'history': {}}
 		else:
 			with open('config.json', 'r') as config_file:
 				config = json.load(config_file)
-				if key_dict['consumer_key'] not in config.keys():
-					config[consumer_key] = {}
 
-		config[consumer_key]['app_name'] = app_name
-		for key, val in key_dict.items():
-			config[consumer_key][key] = val
-
+		config['users'][username] = password
 		with open('config.json', 'w') as config_file:
 			json.dump(config, config_file, indent=4)
 
 		return True
 
-	def sign_in(self, app_name, key_dict, remember_login):
-		if app_name == '':
-			app_name = key_dict['consumer_key'][0:6]
+	def sign_in(self, username, password, remember_login, refresh_cookies):
+		self.username = username
+		self.password = password
+		auth_success = self.authenticate(refresh_cookies)
 
-		self.consumer_key = key_dict['consumer_key']
-		self.consumer_secret = key_dict['consumer_secret']
-		self.user_token = key_dict['user_token']
-		self.user_secret = key_dict['user_secret']
-
-		auth_success = self.authenticate()
-
-		print(f'remember_login: {remember_login}, type: {type(remember_login)}')
+		# DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG 
+		self.authenticated = True
 		if self.authenticated and remember_login:
-			self.application_name = app_name
-
-			success = self.store_login(app_name, key_dict)
+			success = self.store_login(username, password)
 			if success:
 				print('Login stored into config file successfully!')
 
 		return auth_success
 
-	def authenticate(self):
+	def authenticate(self, refresh_cookies):
 		try:
-			RETURN_URL = 'http://localhost:8000'
-			authentication = linkedin.LinkedInDeveloperAuthentication(
-				self.consumer_key, self.consumer_secret, 
-				self.user_token, self.user_secret, 
-				RETURN_URL, linkedin.PERMISSIONS.enums.values()
-			)
-				
-			sg.popup(f'Authentication: {authentication}')
-
+			self.application = Linkedin(self.username, self.password, debug=True, refresh_cookies=refresh_cookies)
 			self.authenticated = True
-			return linkedin.LinkedInApplication(authentication)
-
+			return True
 		except Exception as error:
-			error_args = error.args
-
-			if 'BAD_EMAIL' in error_args:
-				sg.popup('Incorrect email: try again.')
-			elif 'CHALLENGE' in error_args:
-				sg.popup('Error: LinkedIn requires a sign-in challenge.')
+			logging.exception(error)
+			if 'BAD_EMAIL' in error.args:
+				sg.popup('Incorrect email: try again.', title='Incorrect email', keep_on_top=True)
+			elif 'CHALLENGE' in error.args:
+				sg.popup('Error: LinkedIn requires a sign-in challenge.', title='Linkedin error', keep_on_top=True)
 			else:
-				print(error)
-				print(error_args)
-				sg.popup(f'Unhandled exception: {error_args}')
+				sg.popup(f'Unhandled exception: {traceback.format_exc(error)}', title='Unhandled exception', keep_on_top=True)
 
 			return False
 
-	def store_contact_information(self, contact_dict):
-		pass
+	def store_profile(self, profile, contact_info):
+		def set_diff(dict, full_set):
+			ignored_keys = {key for key in full_set if key not in dict.keys()}
+			return full_set.difference(ignored_keys)
 
-	def display_signin_screen(self, VERSION):
+		profile_keys = {
+			'firstName', 'lastName', 'profile_id', 'headline', 
+			'summary', 'industryName', 'geoCountryName', 'languages'}
+		contact_keys = {'birthdate', 'email_address', 'phone_numbers'}
+
+		profile_keys = set_diff(profile, profile_keys)
+		contact_keys = set_diff(contact_info, contact_keys)
+
+		column_map = {
+			'firstName': 'First name',
+			'lastName': 'Last name',
+			'profile_id': 'Linkedin profile ID',
+			'headline': 'Linkedin headline',
+			'summary': 'Linkedin summary',
+			'industryName': 'Industry',
+			'geoCountryName': 'Location',
+			'languages': 'Languages',
+			'birthdate': 'Birthday',
+			'email_address': 'Email address',
+			'phone_numbers': 'Phone number'
+		}
+
+		profile_dict = {}
+
+		for key in profile_keys:
+			if key == 'languages':
+				profile[key] = ','.join(profile['languages'])
+
+			profile_dict[column_map[key]] = profile[key]
+
+		for key in contact_keys:
+			if key == 'phone_numbers':
+				profile[key] = ','.join(contact_info['phone_numbers'])
+			
+			profile_dict[column_map[key]] = contact_info[key]
+
+		field_names = [key for key in profile_dict.keys()]
+		if not os.path.isfile(self.sheet_path):
+			with open(self.sheet_path, 'w', newline='') as csv_file:
+				csv.DictWriter(csv_file, fieldnames=field_names).writeheader()
+				print(f'Created file: {self.sheet_path}')
+
+		if not self.history.add(profile_dict['Linkedin profile ID']):
+			sg.popup(f'This profile has already been added: avoiding duplicate.', font=('Helvetica', 14), title='Duplicate', keep_on_top=True)
+			print(f'Duplicate detected ({profile_dict["Linkedin profile ID"]})\n')
+			return
+
+		with open(self.sheet_path, 'a', newline='') as csv_file:
+			csv.DictWriter(csv_file, fieldnames=field_names).writerow(profile_dict)
+			print(f'Stored {profile_dict["Linkedin profile ID"]}\n')
+			self.parsed += 1
+			self.total_parsed += 1
+
+	def display_signin_screen(self):
 		layout = [
-			[sg.Text('LinkedIn developer authentication', font=('Helvetica Bold', 14))],
-			[sg.Text('Application name\t', font=('Helvetica', 14)), sg.InputText(key="name")],
-			[sg.Text('Consumer key\t', font=('Helvetica', 14)), sg.InputText(key="consumer_key")],
-			[sg.Text('Consumer secret\t', font=('Helvetica', 14)), sg.InputText(key="consumer_secret")],
-			[sg.Text('User token\t', font=('Helvetica', 14)), sg.InputText(key="user_token")],
-			[sg.Text('User secret\t', font=('Helvetica', 14)), sg.InputText(key="user_secret")],
+			[sg.Text('Sign in to LinkedIn to continue', font=('Helvetica Bold', 14))],
+			[sg.Text('Username (email)\t', font=('Helvetica', 14)), sg.InputText(key="username")],
+			[sg.Text('Password\t\t', font=('Helvetica', 14)), sg.InputText(key="password")],
 			[	
 				sg.Text('Select a stored login', font=('Helvetica', 14)),
 				sg.Listbox(
-					self.load_configuration(), select_mode='LISTBOX_SELECT_MODE_SINGLE', size=(40, 2 + len(self.load_configuration())),
-					key='-CONSUMER_KEY-', enable_events=True
+					self.load_configuration(), select_mode='LISTBOX_SELECT_MODE_SINGLE', 
+					enable_events=True, size=(40, 1 + len(self.load_configuration())),
+					key='-USERNAME-', no_scrollbar=True
 					)
 			],
 			[
 				sg.Button('Sign in', font=('Helvetica', 14)), 
-				sg.Checkbox('Remember me', key='remember')
-			]
-		] # [sg.Output(size=(80, 20))]
-
-		return sg.Window(f'Liscrape version {VERSION}', layout=layout, resizable=True, grab_anywhere=True)
-
-	def display_sheet_screen(self, VERSION):
-		layout = [
-			[sg.Text('Choose the Excel sheet to store contact information in', font=('Helvetica Bold', 14))],
-			[sg.FileBrowse(), sg.Input(key="sheet_path")],
-			[sg.Button('OK')]
+				sg.Checkbox('Remember me', key='remember'),
+				sg.Checkbox('Refresh cookies', key='cookies')],
+				[sg.Output(size=(80, 20), font=('Helvetica', 12))]
 		]
 
-		return sg.Window(f'Liscrape version {VERSION}', layout)
+		return sg.Window(f'Liscrape version {self.version}', layout=layout, resizable=True, grab_anywhere=True)
 
-	def display_main_screen(self, VERSION):
+	def display_sheet_screen(self):
 		layout = [
-			[sg.Text(f'Using application {self.application_name}', font=('Helvetica', 14))],
-			[sg.Text('Contact to store (URL)\t', font=('Helvetica', 14)), sg.InputText(key="profile_url")],
-			[sg.Button('Store contact', font=('Helvetica', 14)), sg.Text(f'{self.parsed} contacts stored')],
-			[sg.Text(f'Storing into {self.sheet_path}', font=('Helvetica', 12))],
-			[sg.Text(f'(c) 2020 Icotak Ltd.', font=('Helvetica', 10))]
-		] # [sg.Output(size=(60, 20))]
+			[sg.Text('Choose the Excel sheet to store contact information in (.CSV)', font=('Helvetica Bold', 14))],
+			[sg.FileBrowse(), sg.Input(key="sheet_path")],
+			[sg.Button('OK'), sg.Button('Use default')]
+		]
 
+		return sg.Window(f'Liscrape version {self.version}', layout)
+
+	def display_main_screen(self):
+		layout = [
+			[sg.Text(f'Signed in as {self.username}', font=('Helvetica', 14))],
+			[sg.Text('Contact to store (URL)\t', font=('Helvetica', 14)), sg.InputText(key="profile_url")],
+			[sg.Button('Store contact', font=('Helvetica', 14)), sg.Text(f'{self.parsed} contacts stored (this session)', key='parsed', font=('Helvetica', 14))],
+			[sg.Text(f'Session path: {self.sheet_path}', font=('Helvetica', 14))],
+			[sg.Text(f'Contacts in file: {self.total_parsed}', font=('Helvetica', 12), key='total_parsed')],
+			[sg.Output(size=(60, 20))]
+		]
 		return sg.Window(
-			title=f'Liscrape version {VERSION}', layout=layout, resizable=True, grab_anywhere=True)
+			title=f'Liscrape version {self.version}', layout=layout, resizable=True, grab_anywhere=True)
 
 
 if __name__ == '__main__':
-	VERSION = 0.3
+	debug = False
+
+	# logging
+	log = 'log.log'
+	logging.basicConfig(filename=log,level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+	logging.info(f'Program started')
 	
 	# set theme
 	sg.theme('Reddit')
 
 	# create session, display sign-in screen
 	session = Session()
-	session.window = session.display_signin_screen(VERSION)
 
 	# sign-in eventloop
 	while True:
 		event, values = session.window.read()
-		print(f'event: {event} | values: {values}')
 
 		if event == sg.WIN_CLOSED:
+			logging.info(f'Sign-in window closed')
 			break
 
 		if event == 'Sign in':
-			login_fields = (
-				values['consumer_key'], values['consumer_secret'],
-				values['user_token'], values['user_secret'])
-
-			print(f'Login fields: {login_fields}')
-			print(f'Values: {values}')
-			print(f'Values[consumer_key]: {values["-CONSUMER_KEY-"]}')
-
-			if '' not in login_fields or values['-CONSUMER_KEY-'] != []:
-				if values['-CONSUMER_KEY-'] != []:
-					consumer_key = values['-CONSUMER_KEY-'][0]
-					key_dict = session.load_password_from_config(consumer_key)
+			if values['username'] != '' and values['password'] != '' or values['-USERNAME-'] != []:
+				if values['-USERNAME-'] != []:
+					username = values['-USERNAME-'][0]
+					password = session.load_password_from_config(username)
 					values['remember'] = False
-				else:
-					key_dict = {}
-					for key in ('consumer_key', 'consumer_secret', 'user_token', 'user_secret'):
-						key_dict[key] = values[key]
 
-				session.application = session.sign_in(values['name'], key_dict, values['remember'])
 				print('Signing in...')
+				if not debug:
+					auth_success = session.sign_in(values['username'], values['password'], values['remember'], values['cookies'])
+				else:
+					session.authenticated = True
+					auth_success = True
 
-				if session.application is None:
-					sg.popup('Incorrect login details!')
+				if not auth_success:
+					sg.popup('Incorrect login details!', title='Incorrect login', keep_on_top=True)
 				else: 
-					# successful sign-in, update UI
-					sg.popup('Signed in successfully!')
+					sg.popup('Signed in successfully!', title='Success', keep_on_top=True)
 					session.window.close()
 
 					# request sheet/csv location
-					session.window = session.display_sheet_screen(VERSION)
+					session.window = session.display_sheet_screen()
 					while session.sheet_path == None:
 						event, values = session.window.read()
-						print(f'event: {event} | values: {values}')
 						if event == sg.WIN_CLOSED:
+							if session.sheet_path == None:
+								sg.popup('No Excel sheet defined. Using default path: linkedin_scrape.csv', title='No path defined', keep_on_top=True)
+								session.sheet_path = 'linkedin_scrape.csv'
+							break
+
+						if event == 'Use default':
+							session.sheet_path = 'linkedin_scrape.csv'
 							break
 
 						if values['sheet_path'] != '':
-							if '.xls' in values['sheet_path'] or '.xlsx' in values['sheet_path']:
+							if '.csv' in values['sheet_path'] or '.csv' in values['sheet_path']:
 								session.sheet_path = values['sheet_path']
 
+					session.load_sheet_length()
 					session.window.close()
-
-					# main screen
-					session.window = session.display_main_screen(VERSION)
+					session.window = session.display_main_screen()
 					break
 			else:
-				sg.popup('Please enter your login details!')
-
-		print('You entered ', values)
-
-	if session.sheet_path == None and session.authenticated:
-		session.window.close()
-		sg.popup('Error: no Excel sheet path defined!')
+				sg.popup('Please enter your login details!', title='Incorrect login', keep_on_top=True)
 
 	# main eventloop
-	while True:
-		event, values = session.window.read()
-		if event == sg.WIN_CLOSED:
-			break
+	try:
+		while True and session.authenticated:
+			event, values = session.window.read()
+			if event == sg.WIN_CLOSED:
+				logging.info(f'Main window closed')
+				session.history.store()
+				session.window.close()
+				logging.info(f'Exiting main event loop gracefully')
+				break
 
-		if event == 'Store contact' and 'profile_url' in values:
-			print(f'Loading {values["profile_url"]}...')
-			profile = values['profile_url'].split('/')[-1]
+			if event == 'Store contact' and 'profile_url' in values:
+				print(f'Loading {values["profile_url"]}...')
+				profile = values['profile_url'].split('/')[-1]
 
-			profile = session.application.get_profile(profile)
-			print(profile)
-			print('\n\n')
+				validity_status, time_until_next = session.history.check_validity()
+				if validity_status:
+					if not debug:
+						profile = session.application.get_profile(profile)
+						contact_info = session.application.get_profile_contact_info(profile)
+						session.store_profile(profile, contact_info)
+					else:
+						profile = {'lastName': 'Meikäläinen', 'firstName': 'Matti', 'industryName': 'Professional retard', }
+						contact_info = {'email_address': 'matti@meikalainen.com', 'websites': ['matti.fi'], 'twitter': '@matti', 'phone_numbers': ['+358400123456']}
+						session.store_profile(profile, contact_info)
 
-			contact_info = session.application.get_profile_contact_info(profile)
-			print(contact_info)
+					# clear input
+					session.window['profile_url'].update('')
+					session.window['parsed'].update(f'{session.parsed} {"contact" if session.parsed == 1 else "contacts"} stored (this session)')
+					session.window['total_parsed'].update(f'Contacts in file: {session.total_parsed}')
 
+					print(f'Call count: {session.history.call_count}')
+				else:
+					sg.popup(f'API call limit reached. Try again in {time_until_next}.', font=('Helvetica', 14), title='Limit reached', keep_on_top=True)
+					logging.info(f'API call limit reached: time until next call {time_until_next}. Limit: {session.history.hourly_limit} calls per hour.')
 
-	session.window.close()
-
-
-
+	except Exception as error:
+		logging.exception(error)
+		session.history.store()
+		session.window.close()
